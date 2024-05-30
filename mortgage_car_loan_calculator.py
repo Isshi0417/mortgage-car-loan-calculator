fd03e3fe0e4c3b266463e5c3af5053189afb366d
Sho Ishida <isshi0417@gmail.com>
2024-05-30 15:16:15 +0900

    Complete barebones

--- mortgage_car_loan_calculator.py
+++ mortgage_car_loan_calculator.py
@@ -1,6 +1,16 @@
 # Welcome message
+print("Welcome to the Mortgage/Car Loan Calculator!")
 # Explain what the program does
-# Ask for the total loan mount
+print("This program takes the total loan, monthly interest rate, and " 
+      + "duration of the loan(in months) to calculate the monthly payment.")
+# Ask for the total loan amount
+total_loan = float(input("Please enter your total loan amount: "))
 # Ask for the monthly interest rate
+monthly_interest = float(input("Please enter your monthly interst rate: "))
 # Ask for the duration of the loan (in months)
+duration = int(input("Please enter the duration of the loan(in months): "))
+# Do the math
+monthly_payment = total_loan * (monthly_interest / 
+                                (1 - (1 + monthly_interest) ** (-duration)))
 # Print monthly payment
+print("Your monthly payment is", monthly_payment)
