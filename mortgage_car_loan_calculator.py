e72555cafe9eebc32a98c2ea3e2712ad56a2b152
Sho Ishida <isshi0417@gmail.com>
2024-05-31 00:55:04 +0900

    Implement cleaner formatting with arrows and spacing

--- mortgage_car_loan_calculator.py
+++ mortgage_car_loan_calculator.py
@@ -1,25 +1,59 @@
 import json
 
+# Read JSON file
 with open('messages.json', 'r') as file:
     MESSAGES = json.load(file)
 
-def prompt(key):
+# Methods
+def prompt(key, format = "\n"):
     message = MESSAGES[key]
-    print(f"=> {message}")
+    print(f" -> {message}", end = format)
 
+def invalid_number(number_str):
+    try:
+        float(number_str)
+    except ValueError:
+        return True
+    
+    return False
+
+# Calculator Program
 while True:
     prompt("welcome")
     prompt("explanation")
-    prompt("ask_loan")
-    prompt("ask_interest")
-    monthly_interest = float(input())
-    prompt("ask_duration")
-    duration = int(input())
-    monthly_payment = total_loan * (monthly_interest / 
-                                    (1 - (1 + monthly_interest) ** (-duration)))
-    prompt("result")
-    print(f"=> {monthly_payment}")
+    print("\n")
 
+    prompt("ask_loan")
+    total_loan = input()
+
+    while invalid_number(total_loan):
+        prompt("invalid_loan")
+        total_loan = input()
+
+    print("\n")
+    prompt("ask_interest")
+    monthly_interest = input()
+
+    while invalid_number(monthly_interest):
+        prompt("invalid_interest")
+        monthly_interest = input()
+
+    print("\n")
+    prompt("ask_duration")
+    duration = input()
+
+    while invalid_number(duration):
+        prompt("invalid_duration")
+        duration = input()
+
+    monthly_payment = float(total_loan) * (float(monthly_interest) / 
+                    (1 - (1 + float(monthly_interest)) ** (-float(duration))))
+    
+    print("\n")
+    prompt("result", " ")
+    print(monthly_payment)
+
+    print("\n")
     prompt("another_one")
     answer = input().lower()
     if (answer and answer[0] != "y") or answer == "":
