1192a8ff592d97a3b5769691939b22c83faa4e9b
Sho Ishida <isshi0417@gmail.com>
2024-05-30 15:04:39 +0900

    Complete pseudocode

---
+++ mortgage_car_loan_calculator.py
@@ -0,0 +1,6 @@
+# Welcome message
+# Explain what the program does
+# Ask for the total loan mount
+# Ask for the monthly interest rate
+# Ask for the duration of the loan (in months)
+# Print monthly payment
