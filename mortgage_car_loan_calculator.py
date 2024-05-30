7a0608b3aba01b9d3da5736a91712e916271ad3b
Sho Ishida <isshi0417@gmail.com>
2024-05-30 17:40:05 +0900

    Complete JSON integration

--- mortgage_car_loan_calculator.py
+++ mortgage_car_loan_calculator.py
@@ -1,16 +1,20 @@
+import json
+
+with open('messages.json', 'r') as file:
+    MESSAGES = json.load(file)
+
 # Welcome message
-print("Welcome to the Mortgage/Car Loan Calculator!")
+print(MESSAGES["welcome"])
 # Explain what the program does
-print("This program takes the total loan, monthly interest rate, and " 
-      + "duration of the loan(in months) to calculate the monthly payment.")
+print(MESSAGES["explanation"])
 # Ask for the total loan amount
-total_loan = float(input("Please enter your total loan amount: "))
+total_loan = float(input(MESSAGES["ask_loan"]))
 # Ask for the monthly interest rate
-monthly_interest = float(input("Please enter your monthly interst rate: "))
+monthly_interest = float(input(MESSAGES["ask_interest"]))
 # Ask for the duration of the loan (in months)
-duration = int(input("Please enter the duration of the loan(in months): "))
+duration = int(input(MESSAGES["ask_duration"]))
 # Do the math
 monthly_payment = total_loan * (monthly_interest / 
                                 (1 - (1 + monthly_interest) ** (-duration)))
 # Print monthly payment
-print("Your monthly payment is", monthly_payment)
+print(MESSAGES["result"], monthly_payment)
