abeffedd17a5aef2e27d1a684e2f4233fa91e30e
Sho Ishida <isshi0417@gmail.com>
2024-05-31 23:37:30 +0900

    Fix Python convention violations

--- mortgage_car_loan_calculator.py
+++ mortgage_car_loan_calculator.py
@@ -1,28 +1,32 @@
+"""Import JSON module"""
 import json
 
 # Read JSON file
-with open('messages.json', 'r') as file:
+with open('messages.json', 'r', encoding="utf-8") as file:
     MESSAGES = json.load(file)
 
 # Methods
-def prompt(key, format = "\n"):
+def prompt(key, space = "\n"):
+    """Prints messages in JSON file based on key"""
     message = MESSAGES[key]
-    print(f" -> {message}", end = format)
+    print(f" -> {message}", end = space)
 
 def invalid_float(number_str):
+    """Checks if param is a valid number"""
     try:
         float(number_str)
     except ValueError:
         return True
-    
+
     return False
 
 def invalid_int(number_str):
+    """Checks if param is an integer"""
     try:
         int(number_str)
     except ValueError:
         return True
-    
+
     return False
 
 # Calculator Program
@@ -36,7 +40,7 @@ print(MESSAGES["divider"])
 prompt("ask_currency_format")
 currency_format = input().lower()[0]
 
-while currency_format != ("y") and currency_format != ("n"):
+while currency_format not in ('y', 'n'):
     prompt("invalid_answer")
     currency_format = input().lower()
 
@@ -48,24 +52,23 @@ if currency_format == "y":
     while invalid_int(decimal_places):
         prompt("invalid_integer")
         decimal_places = input()
-    
-decimal_places = int(decimal_places)
+
+    decimal_places = int(decimal_places)
 
 print(MESSAGES["divider"])
 
 # Calculations
 while True:
 
-    # Total loan 
+    # Total loan
     prompt("ask_loan")
     total_loan = input()
 
     while invalid_float(total_loan):
         prompt("invalid_loan")
         total_loan = input()
-        
-    total_loan = float(total_loan)
 
+    total_loan = float(total_loan)
     print(MESSAGES["divider"])
 
     # Monthly interest
@@ -78,7 +81,7 @@ while True:
 
     monthly_interest = float(monthly_interest)
 
-    if (monthly_interest >= 1):
+    if monthly_interest >= 1:
         monthly_interest /= 100
 
     print(MESSAGES["divider"])
@@ -91,18 +94,18 @@ while True:
         prompt("invalid_duration")
         duration = input()
 
-    duration = float(duration) 
+    duration = float(duration)
 
     # Monthly payment
     monthly_payment = total_loan * monthly_interest / \
                         (1 - (1 + monthly_interest) ** (-duration))
-    
+
     print(MESSAGES["divider"])
     prompt("result", " ")
 
     if currency_format == "y":
         print(round(monthly_payment, decimal_places))
-    else: 
+    else:
         print(round(monthly_payment))
 
     print(MESSAGES["divider"])
@@ -113,5 +116,5 @@ while True:
 
     if (answer and answer[0] != "y") or answer == "":
         break
-    else:
-        print(MESSAGES["divider"])
+
+    print(MESSAGES["divider"])
